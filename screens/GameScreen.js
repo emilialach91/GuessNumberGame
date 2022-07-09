db6884ef234959e518c24b3265ed506f8847cd34
Emilia <emilia.lach@electroneum.com>
2022-07-09 21:08:17 +0100

    Added: game screen with guessing the number logic

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,14 +1,67 @@
-import React from 'react'
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useState, useRef } from 'react'
+import { StyleSheet, View, Text, Button, Alert } from 'react-native';
+import Card from '../components/Card';
+import NumberContainer from '../components/NumberContainer';
 
+
+const generateRandomBetween = (min, max, exclude) => {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+	if (rndNum === exclude) {
+		return generateRandomBetween(min, max, exclude);
+	} else {
+		return rndNum;
+	}
+}
 
 const GameScreen = props => {
+	const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 100, props.userChoice));
+
+	const currentLow = useRef(1);
+	const currentHigh = useRef(100)
+
+	const nextGuessHandler = direction => {
+		if ((direction === 'lower' && currentGuess < props.userChoice) || (direction === 'greater' && currentGuess > props.userChoice)) {
+			Alert.alert('Don\'t lie!', 'You know that this is wrong...', [{ text: 'Sorry!', style: 'cancel' }])
+			return;
+		};
+		if (direction === 'lower') {
+			currentHigh.current = currentGuess;
+		} else {
+			currentLow.current = currentGuess;
+		}
+
+		const nextGuess = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess);
+		setCurrentGuess(nextGuess);
+	}
+
 	return (
-		<View></View>
+		<View style={styles.screen}>
+			<Text>Opponent's Guess</Text>
+			<NumberContainer>{currentGuess}</NumberContainer>
+			<Card style={styles.buttonsContainer}>
+				<Button title="LOWER" onPress={nextGuessHandler.bind(this, 'lower')} />
+				<Button title="GREATER" onPress={nextGuessHandler.bind(this, 'greater')} />
+			</Card>
+		</View>
 	);
 }
 
 const styles = StyleSheet.create({
+	screen: {
+		flex: 1,
+		padding: 10,
+		alignItems: 'center',
+	},
+	buttonsContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		marginTop: 20,
+		width: 300,
+		maxWidth: '80%',
+	}
 
 });
 
