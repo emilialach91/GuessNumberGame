d8c259b9997521273f6d12076b5bfd6a1ea43eeb
Emilia <emilia.lach@electroneum.com>
2022-07-28 21:57:16 +0100

    Change menu icon, changed menu visibility, styling tweaks

--- components/Menu.js
+++ components/Menu.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { View, StyleSheet, Alert } from 'react-native';
 import { Menu, Provider, Divider } from 'react-native-paper';
-import { AntDesign } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
 const MenuExample = props => {
 	const [visible, setVisible] = useState(false);
@@ -9,7 +9,8 @@ const MenuExample = props => {
 	const closeMenu = () => setVisible(false);
 	const openMenu = () => setVisible(true);
 
-	const alertButtonHandler = () => {
+	const buttonEndGameHandler = () => {
+		closeMenu()
 		Alert.alert(
 			"End the Game",
 			"Are you sure you want to end this game?",
@@ -18,38 +19,43 @@ const MenuExample = props => {
 				{ text: "Yes", onPress: () => props.onEndGame() }
 			]
 		);
+	}
+
+	const buttonRulesHandler = () => {
 		closeMenu()
+		Alert.alert(
+			"Rules",
+			"Choose a number from 0 to 99 and check if the computer can guess your number in seven moves.",
+			[
+				{ text: "OK", onPress: () => closeMenu() }
+			]
+		);
 	}
 
 	return (
-		<Provider style={styles.container}>
-			<View>
-				<Menu
-					visible={visible}
-					onDismiss={closeMenu}
-					anchor={
-						<AntDesign name="bars" size={24} color="black" onPress={openMenu} />
-					}>
-					<Menu.Item
-						onPress={() => {
-							Alert.alert(
-								"Rules",
-								"Choose a number from 0 to 99 and check if the computer can guess your number in seven moves.",
-								[
-									{ text: "OK", onPress: () => closeMenu() }
-								]
-							);
-						}}
-						title="Rules"
-					/>
-					<Divider />
-					<Menu.Item
-						onPress={() => alertButtonHandler()}
-						title="End the Game"
-					/>
-				</Menu>
-			</View>
-		</Provider>
+		<View style={styles.container}>
+			<Provider style={styles.provider}>
+				<View>
+					<Menu
+						style={styles.menu}
+						visible={visible}
+						onDismiss={() => closeMenu()}
+						anchor={
+							<Ionicons name="settings-outline" size={22} color="black" onPress={openMenu} />
+						}>
+						<Menu.Item
+							onPress={() => buttonRulesHandler()}
+							title="Rules"
+						/>
+						<Divider />
+						<Menu.Item
+							onPress={() => buttonEndGameHandler()}
+							title="End the Game"
+						/>
+					</Menu>
+				</View>
+			</Provider>
+		</View>
 	);
 };
 
@@ -57,15 +63,20 @@ export default MenuExample;
 
 const styles = StyleSheet.create({
 	container: {
-		padding: 50,
-		flexDirection: 'row',
-		justifyContent: 'center',
-		backgroundColor: 'red',
-		width: 20,
-		height: 20,
-		zIndex: 100,
-		top: 0
+		height: 40,
+		width: 40,
+		margin: 10,
+		zIndex: 1000
 	},
+	provider: {
+		height: 40,
+		width: 40,
+	},
+	menu: {
+		zIndex: 1000,
+		width: 140,
+		transform: [{ translateY: -80 }]
+	}
 });
 
 
